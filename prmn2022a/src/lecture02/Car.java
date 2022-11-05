7bd3cdb866d0b180b9b1e662bc5f6204fdda7e5f
KitaYuusuke <cist.b221.y.kita@gmail.com>
2022-11-05 13:30:05 +0900

    Exercise2_2追加

---
+++ prmn2022a/src/lecture02/Car.java
@@ -0,0 +1,19 @@
+package lecture02;
+
+public class Car {
+    int fuel;
+
+    Car(){
+        this.fuel = 0;
+    }
+
+    void run(){
+        if(fuel == 0){
+            System.out.println("燃料が足りないため走れませんでした。");
+        }
+        else{
+            fuel--;
+            System.out.println("燃料を1消費して走りました。");
+        }
+    }
+}
