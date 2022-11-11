526f1a3066902d8f43c0acf7006ddff9e71676ae
KitaYuusuke <cist.b221.y.kita@gmail.com>
2022-11-12 00:38:49 +0900

    課題3-3追加

---
+++ prmn2022a/src/lecture03/Exercise3_4.java
@@ -0,0 +1,18 @@
+package lecture03;
+import java.util.ArrayList;
+
+public class Exercise3_4 {
+    public static void main(String[] args) {
+        ArrayList<Vegetable> vege = new ArrayList<>();
+
+        vege.add(new Vegetable("にんじん",117));
+        vege.add(new Vegetable("たまねぎ",120));
+        vege.add(new Vegetable("じゃがいも",154));
+
+        for(int i=0;i< vege.size();i++){
+            vege.get(i).print();
+        }
+
+
+    }
+}
