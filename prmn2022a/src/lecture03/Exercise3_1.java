ab9f7e53dd486ed306e52d5be876201c10704316
KitaYuusuke <cist.b221.y.kita@gmail.com>
2022-11-11 23:46:53 +0900

    課題3-1追加

---
+++ prmn2022a/src/lecture03/Exercise3_1.java
@@ -0,0 +1,13 @@
+package lecture03;
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("任意の文字列を入力してください：");
+        String mozi = input.nextLine();
+        System.out.println("”" + mozi + "”" + "と入力されました");
+
+    }
+}
